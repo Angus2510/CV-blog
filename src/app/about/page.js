15c2011d933050ff9976d57ce0e9b61b4f9807fc
Angus2510 <124569463+Angus2510@users.noreply.github.com>
2023-10-20 19:13:41 +0800

    yes

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -21,7 +21,7 @@ const About = () => {
         <div className={styles.imgText}>
           <h1 className={styles.imgTitle}>Nomadic Tales</h1>
           <h2 className={styles.imgDesc}>
-            From the good, bad and need to know
+            From the good, bad and the need to know
           </h2>
         </div>
       </div>
@@ -29,19 +29,45 @@ const About = () => {
         <div className={styles.item}>
           <h1 className={styles.title}>What I am About</h1>
           <p className={styles.desc}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. A veniam
-            adipisci, vel enim maiores inventore, ratione dignissimos dolor nemo
-            facilis nesciunt <br /> consequatur eaque quas suscipit doloribus
-            expedita repudiandae soluta deleniti.
+            I'm all about the thrill of meeting new people and diving headfirst
+            into the vibrant tapestry of different cultures and personalities.
+            But let me share a bit of my story – I grew up in the heart of
+            Botswana, in a town called Maun. Picture this: the incredible
+            Okavango Delta was practically in my backyard, and I couldn't have
+            asked for a more exhilarating playground. Surf's Up and Nose in a
+            Book When I'm not exploring the world, you'll often find me chasing
+            waves on my trusty surfboard. Riding the swell of the ocean is my
+            idea of pure bliss. The rhythm of the waves and the feel of the
+            board beneath my feet is my ultimate happy place. And when it's time
+            to dry off, I'm a devoted bookworm. I find solace in the pages of a
+            good novel, and there's no adventure quite like diving into the
+            world of a well-crafted story. My love for reading is right up there
+            with my passion for surfing. Challenge Junkie and Results Wrangler
+            Challenges? Bring 'em on!
           </p>
         </div>
         <div className={styles.item}>
-          <h1 className={styles.title}>What I am About</h1>
           <p className={styles.desc}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. A veniam
-            adipisci, vel enim maiores inventore, ratione dignissimos dolor nemo
-            facilis nesciunt <br /> consequatur eaque quas suscipit doloribus
-            expedita repudiandae soluta deleniti.
+            Growing up in the wild heart of Botswana taught me a thing or two
+            about tackling obstacles head-on. Whether it's conquering a monster
+            wave or tackling a tricky problem at work, I'm always up for the
+            challenge. But here's the fun twist: I'm not just about personal
+            success; I get a kick out of helping others overcome their workplace
+            hurdles. Sharing knowledge, fostering growth, and building a strong
+            team is where I find my groove. In any group, I naturally assume the
+            role of a leader, steering us toward success. Strategic Thinker and
+            Quick Learner While I'm all about the adventure and fun, I'm also a
+            strategic thinker. When life throws curveballs, I love using my
+            initiative to find creative solutions. I'm not afraid of the
+            unexpected; I see it as a chance to explore uncharted waters. The
+            best part? I'm a quick learner. Absorbing and retaining vast amounts
+            of information is a skill I'm proud of. So, whether it's mastering a
+            new surf technique, diving into a new culture, or finding innovative
+            solutions, I'm always ready for the next thrilling challenge. So,
+            buckle up and join me on this wild ride as we surf the waves, dive
+            into amazing stories, conquer life's challenges together, and relive
+            the adventures of my Okavango Delta backyard. Let's ride the
+            adventure of a lifetime! 🌊📚🚀
           </p>
 
           <Button url="/contact" text="Contact" />
--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -2,29 +2,28 @@ import { NextResponse } from "next/server";
 import connect from "@/utils/db";
 import Post from "@/models/Post";
 
-export const GET = async (request, { params }) => {
-  const { id } = params;
+export const GET = async (request) => {
+  const url = new URL(request.url);
+  const username = url.searchParams.get("username");
 
   try {
     await connect();
-
-    const post = await Post.findById(id);
-
-    return new NextResponse(JSON.stringify(post), { status: 200 });
+    const query = username ? { username } : {};
+    const posts = await Post.find(query);
+    return new NextResponse(JSON.stringify(posts), { status: 200 });
   } catch (err) {
     return new NextResponse("Database Error", { status: 500 });
   }
 };
 
-export const DELETE = async (request, { params }) => {
-  const { id } = params;
+export const POST = async (request) => {
+  const body = await request.json();
+  const newPost = new Post(body);
 
   try {
     await connect();
-
-    await Post.findByIdAndDelete(id);
-
-    return new NextResponse("Post has been deleted", { status: 200 });
+    await newPost.save();
+    return new NextResponse("Post has been created", { status: 201 });
   } catch (err) {
     return new NextResponse("Database Error", { status: 500 });
   }
